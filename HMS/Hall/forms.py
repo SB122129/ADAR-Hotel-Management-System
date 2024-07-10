862ed5fd27597b40ff4d543836649afc31061cd3
Samson Berhanu <samiberhanu122129@gmail.com>
2024-07-10 16:39:48 +0300

    24th commit

--- HMS/Hall/forms.py
+++ HMS/Hall/forms.py
@@ -2,6 +2,9 @@
 
 from django import forms
 from .models import Hall_Booking
+from django import forms
+from django.core.exceptions import ValidationError
+
 
 class BookingForm(forms.ModelForm):
     class Meta:
@@ -14,8 +17,24 @@ class BookingForm(forms.ModelForm):
             'end_time': forms.TimeInput(attrs={'type': 'time'}),
         }
 
+
 class CheckAvailabilityForm(forms.Form):
     start_date = forms.DateField(widget=forms.TextInput(attrs={'type': 'date'}))
     end_date = forms.DateField(required=False, widget=forms.TextInput(attrs={'type': 'date'}))
     start_time = forms.TimeField(widget=forms.TextInput(attrs={'type': 'time'}))
     end_time = forms.TimeField(widget=forms.TextInput(attrs={'type': 'time'}))
+
+    def clean(self):
+        cleaned_data = super().clean()
+        start_date = cleaned_data.get('start_date')
+        end_date = cleaned_data.get('end_date')
+        start_time = cleaned_data.get('start_time')
+        end_time = cleaned_data.get('end_time')
+
+        if end_date and end_date < start_date:
+            raise ValidationError("End date cannot be before start date.")
+
+        if end_date == start_date and end_time < start_time:
+            raise ValidationError("End time cannot be before start time when start and end dates are the same.")
+
+        return cleaned_data
