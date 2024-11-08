f202fdf1df4883c43bbcc0de605afbe61ade4ea9
Samson Berhanu <samiberhanu122129@gmail.com>
2024-11-08 17:37:31 +0300

    last commit

--- HMS/HMS/settings.py
+++ HMS/HMS/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-kvg73ir99on-u4xg=d#))cu8bfm*ig8(hvrb8xj$n$h=@5=(bj'
-
+JBKJHKHHLLKHKLJLK
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
