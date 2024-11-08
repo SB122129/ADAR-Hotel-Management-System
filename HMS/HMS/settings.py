25a282c8b7d08b967f4d6f66ec4a73826e30e5d0
Samson Berhanu <samiberhanu122129@gmail.com>
2024-11-08 17:46:32 +0300

    last commit

--- HMS/HMS/settings.py
+++ HMS/HMS/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
-
+#vhvjh
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-kvg73ir99on-u4xg=d#))cu8bfm*ig8(hvrb8xj$n$h=@5=(bj'
 # SECURITY WARNING: don't run with debug turned on in production!
